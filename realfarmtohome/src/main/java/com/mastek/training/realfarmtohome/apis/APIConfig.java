5f73899d716a10fdad2fded465d5e261290ed88e
ryanvanbaardwijk <ryan.baardwijk@mastek.com>
2019-07-30 10:15:03 +0100

    Fixed farmertest; added CORS filter

---
+++ realfarmtohome/src/main/java/com/mastek/training/realfarmtohome/apis/APIConfig.java
@@ -0,0 +1,16 @@
+package com.mastek.training.realfarmtohome.apis;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+//create the jersey config class
+@Component
+public class APIConfig extends ResourceConfig {
+	public APIConfig() {
+		//register each service
+		register(CORSFilter.class);
+		register(CustomerService.class);
+		register(FarmerService.class);
+		register(ProductService.class);
+	}
+}
---
+++ realfarmtohome/src/main/java/com/mastek/training/realfarmtohome/apis/CORSFilter.java
@@ -0,0 +1,19 @@
+package com.mastek.training.realfarmtohome.apis;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+public class CORSFilter implements ContainerResponseFilter{
+	
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		headers.add("Access-Control-Allow-Headers", "X-Request-With, Content-Type");		
+	}
+}
