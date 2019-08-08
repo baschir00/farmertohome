cd1b8e6a3324bb52559a9fea1792210de520e84d
justinben97 <50618383+justinben97@users.noreply.github.com>
2019-08-08 13:36:42 +0100

    googleMaps

--- farmtohomeui/src/app/farm.service.ts
+++ farmtohomeui/src/app/farm.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { FarmComponent } from './farm/farm.component';
+//import { FarmComponent } from './farm/farm.component';
 import { Product } from './product';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
