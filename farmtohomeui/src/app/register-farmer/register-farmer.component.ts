f25f0ec050f113f1fddc15d9960a8e1c01db8bbf
ryanvanbaardwijk <ryan.baardwijk@mastek.com>
2019-08-14 16:02:26 +0100

    Validatios and messages

--- farmtohomeui/src/app/register-farmer/register-farmer.component.ts
+++ farmtohomeui/src/app/register-farmer/register-farmer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FarmService } from '../farm.service';
+import { Farmer } from "../farmer";
+import { NgModel } from "@angular/forms";
 
 
 @Component({
@@ -9,28 +11,48 @@ import { FarmService } from '../farm.service';
   styleUrls: ['./register-farmer.component.css']
 })
 export class RegisterFarmerComponent implements OnInit {
+
+  farmer: Farmer;
+  farmerPasswordConfirm: string;
   isFarmerFormValid: boolean;
   invalidFormMessage: string;
 
-  constructor(private farmSvc: FarmService) { }
+  constructor(private farmSvc: FarmService) {
+    this.farmer = { farmerId: 0, farmerName: "", farmerEmail: "", farmerLocation: "", farmerPassword: "" };
+    this.farmerPasswordConfirm = "";
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  }
 
   addFarmer(farmerName, farmerLocation, farmerEmail, farmerPassword) {
-    farmerName = farmerName.value;
-    farmerLocation = farmerLocation.value;
-    farmerEmail = farmerEmail.value;
-    farmerPassword = farmerPassword.value;
-    
+    farmerName = this.farmer.farmerName;
+    farmerLocation = this.farmer.farmerLocation;
+    farmerEmail = this.farmer.farmerEmail;
+    farmerPassword = this.farmer.farmerPassword;
+
+
     console.log('Registering farmer : addFarmer');
     console.log(farmerName, farmerLocation, farmerEmail, farmerPassword);
 
-    if (farmerName.length < 2) {
+    if (farmerName.length < 1) {
+      console.log(1)
       this.isFarmerFormValid = false;
       this.invalidFormMessage =
-        'Product Name numst be greater then 2 characters';
+        'Name field is required';
+    } else if (farmerEmail.length < 1) {
+      console.log(2)
+      this.isFarmerFormValid = false;
+      this.invalidFormMessage =
+        'Email field is required';
+    } else if (farmerPassword !== this.farmerPasswordConfirm) {
+      console.log(3)
+      this.isFarmerFormValid = false;
+      this.invalidFormMessage =
+        'Passwords don\'t match';
     } else {
-
+      console.log(4)
       this.farmSvc.registerFarmer(farmerName, farmerLocation, farmerEmail, farmerPassword)
         .subscribe(
           responseDep => {
